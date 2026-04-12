20527d3ef3924bfc8a781a5e390404691fb28e0f
masterofray <aryanto.dandan@gmail.com>
2026-04-12 22:04:34 +0900

    add the xgboost LTR and FPGrowth

--- setup.py
+++ setup.py
@@ -1,1 +1,22 @@
-pass
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+import numpy as np
+
+ext_modules = [
+    Extension(
+        "fpgrowth_core",
+        sources=["./src/models/fpgrowth/fpgrowth_core.pyx"],
+        include_dirs=[np.get_include()],
+        extra_compile_args=['-fopenmp'],
+        extra_link_args=['-fopenmp'],
+        annotate=True,
+        quiet=True,
+    )
+]
+
+setup(
+    ext_modules=cythonize(ext_modules, 
+                          language_level=3,
+                          annotate=True),
+    zip_safe=False,
+)
---
+++ src/models/ltr_lgbm/readme.md
@@ -0,0 +1,26 @@
+# How to Run Script
+
+```bash
+python lightgbm_train.py
+```
+
+---
+
+# View MLflow
+
+```bash
+mlflow ui
+```
+
+Then open:
+
+```
+http://localhost:5000
+```
+
+You will see:
+
+* experiments
+* metrics
+* plots
+* model artifacts
