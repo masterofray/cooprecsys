580136f736948885a39f4faf5b5e43110252a384
masterofray <aryanto.dandan@gmail.com>
2026-05-01 20:00:28 +0900

    add the module report to render jinja2 html with javascript and css

---
+++ src/models/ltr_lgbm/report/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+__author__     = "Aryanto"
+__copyright__  = "Copyright 2026, Masterofray/Rekomendasi Produk Koperasi"
+__credits__    = ["aryanto"]
+__license__    = "GNU_Public"
+__version__    = "0.0.1"
+__maintainer__ = "Aryanto"
+__email__      = "aryanto.dandan@gmail.com"
+__status__     = "Development"
+__created__    = "2026-05-01"
+
+from .renderpot import render_report
+
+__all__ = ["render_report"]
