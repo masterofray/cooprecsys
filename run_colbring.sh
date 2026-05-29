4f49b29bc9987db8ced7828774f440e0625acc49
masterofray <aryanto.dandan@gmail.com>
2026-05-29 23:50:55 +0900

    update the run_colbring.sh

--- run_colbring.sh
+++ run_colbring.sh
@@ -1,2 +1,16 @@
-cd ./src
-python3 ./models/arycolbring/cysetup.py build_ext --inplace
+#!/usr/bin/env bash
+
+set -e
+
+echo "========================================="
+echo "BUILDING CYTHON MODULES"
+echo "========================================="
+
+cd src
+
+python ./models/arycolbring/cysetup.py build_ext --inplace
+
+echo "========================================="
+echo "BUILD FINISHED"
+echo "========================================="
+
