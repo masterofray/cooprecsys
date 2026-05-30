114a711fcdd7a7b7c5849cafc72a35a303d580ec
masterofray <aryanto.dandan@gmail.com>
2026-05-30 09:46:58 +0900

    Updating the pipeline and run_conbring.sh

--- run_colbring.sh
+++ run_colbring.sh
@@ -7,8 +7,8 @@ echo "BUILDING CYTHON MODULES"
 echo "========================================="
 
 cd src
-
-python ./models/arycolbring/cysetup.py build_ext --inplace
+python3 --version
+python3 ./models/arycolbring/cysetup.py build_ext --inplace
 
 echo "========================================="
 echo "BUILD FINISHED"
